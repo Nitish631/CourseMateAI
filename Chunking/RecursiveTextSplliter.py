1e47af0fd51f8dea2d3302db6d2042508c37945d
Nitish631 <nitishrajbanshi61@gmail.com>
2026-05-02 19:22:59 +0545

    Chunking is completed

--- Chunking/RecursiveTextSplliter.py
+++ Chunking/RecursiveTextSplliter.py
@@ -1,10 +1,13 @@
-from langchain_text_splitters import TokenTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 import os
 path = os.path.join(os.path.dirname(__file__), "notes.txt")
 loader = TextLoader(path, encoding="utf-8")
 docs = loader.load()
-text_splitter = TokenTextSplitter(chunk_size=10, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=20
+)
 
 chunks = text_splitter.split_documents(docs)
 print(chunks[0])
